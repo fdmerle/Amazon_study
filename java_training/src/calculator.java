5e3241b25eca5073bc877087ace773ac2fef41b9
unknown <Dmytro_Moskalenko2@EPPLKRAW0218.budapest.epam.com>
2015-12-04 16:56:57 +0100

    Final test

---
+++ java_training/src/calculator.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Dmytro_Moskalenko2 on 12/4/2015.
+ */
+public class calculator {
+    private int valFirst = 1;
+    private int  valSecond=5;
+
+    public void action(){
+        System.out.println(valFirst+valSecond);
+    }
+}
