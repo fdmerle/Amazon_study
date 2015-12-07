abd092d20be64cec9981e4d41c4be43ce86a8a0b
unknown <Dmytro_Moskalenko2@EPPLKRAW0218.budapest.epam.com>
2015-12-07 12:07:23 +0100

    Final test

--- java_training/src/Test/CaclulatorTest.java
+++ java_training/src/Test/CaclulatorTest.java
@@ -9,26 +9,25 @@ import org.testng.annotations.Test;
  */
 
 public class CaclulatorTest {
-    Calculator calcObj = new Calculator("+", 3, 4);
+    Calculator calcObj = new Calculator();
 
     @Test
     public void testSum() {
 
-        Calculator calcObj = new Calculator("+", 3, 4);
-        Assert.assertEquals(calcObj.action(), 7);
-
+        Assert.assertEquals(calcObj.action("+",7,5), 12);
 
     }
-
     @Test
     public void testDeduct() {
-        calcObj = new Calculator("-", 3, 6);
-        Assert.assertEquals(calcObj.action(), -3);
+
+        Assert.assertEquals(calcObj.action("-",7,5), 2);
+
+    }
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testIllegal() {
+
+        Assert.assertEquals(calcObj.action("/",7,5), 12);
+
     }
 
-    @Test
-    public void testIllegal() {
-        calcObj = new Calculator("/", 3, 6);
-        Assert.assertEquals(calcObj.action(), 000);
-    }
 }
