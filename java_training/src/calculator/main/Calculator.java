d71f98ac58c9c63b8e5534e4cf22629ebfe8291c
unknown <Dmytro_Moskalenko2@EPPLKRAW0218.budapest.epam.com>
2015-12-08 18:00:27 +0100

    Final test

--- java_training/src/calculator/main/Calculator.java
+++ java_training/src/calculator/main/Calculator.java
@@ -1,17 +1,24 @@
 package calculator.main;
 
+import calculator.actions.IActions;
 import calculator.factory.ActionsFactory;
+
+import java.util.HashMap;
 
 /**
  * Created by Dmytro_Moskalenko2 on 12/4/2015.
  */
 public class Calculator {
+    private HashMap<String, IActions> calculatorObj;
+
+    public Calculator() {
+        CationFactoryInit();
+    }
 
     public int action(String opearationMath, int... values) {
-        ActionsFactory actionHashMap = new ActionsFactory();
 
-        if (actionHashMap.returnActions().containsKey(opearationMath)) {
-            return actionHashMap.returnActions().get(opearationMath).returnValue(values);
+        if (this.calculatorObj.containsKey(opearationMath)) {
+            return this.calculatorObj.get(opearationMath).returnValue(values);
         } else {
             throw new IllegalArgumentException();
         }
@@ -19,5 +26,10 @@ public class Calculator {
 
     }
 
+
+    private void CationFactoryInit() {
+        ActionsFactory actionHashMap = new ActionsFactory();
+        this.calculatorObj = actionHashMap.returnActions();
+    }
 }
 
